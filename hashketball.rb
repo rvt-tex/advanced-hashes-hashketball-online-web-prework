8056c3efb482a09a03df79af761febdf8f1a7a48
Ruel Tingling <rvtingling@yahoo.com>
2019-09-16 15:36:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,7 +1,26 @@
 # Write your code here!
 
+def game_hash {
+  :home => [
+    {
+  :team_name => "home_team_name"
+  :colors => "team colors"
+  :players => ["Alan Anderson", "Reggie Evans", "Brook Lopez", "Mason Plumlee", "Jason Terry"]
+  
+  
+  [:number =>"", :shoe =>"", :points =>"", :rebounds =>"", :assists =>"", :steal =>"", :blocks =>"", :slam_dunks =>""]
+  ]
+  
+}
 
 
+
+
+def home_team_name
+  game_hash[:home][:team_name]
+end
+ 
+puts home_team_name
 
 
 
